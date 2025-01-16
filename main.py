491431f821ed380468220b75964ee922d7c8f00d
unendlicherPing2 <167921564+unendlicherPing2@users.noreply.github.com>
2025-01-16 12:42:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import subprocess
+import requests
+from requests.auth import HTTPDigestAuth
+
+PRUSA_CONNECT_API = "https://connect.prusa3d.com"
+PRUSA_CONNECT_API_KEY = ""
+PRINTER_TARGET_PATH = "/autoprint/"
+PRINTER_ID = ""
+FILE_PATH = "./static/temp/biotop.gcode"
 
 app = FastAPI()
 
@@ -17,6 +25,11 @@ async def generate(diameter: float = Form(...)):
     slice_model()
     return FileResponse("static/generate.html", media_type="text/html")
 
+@app.get("/upload")
+async def upload():
+    print(upload_gcode().content)
+    return FileResponse("static/index.html", media_type="text/html")
+
 def build_model(diameter: float):
     subprocess.run(["./lib/FreeCAD_1.0.0/bin/freecadcmd.exe", "build_model.py", str(diameter)])
 
@@ -26,6 +39,17 @@ def slice_model():
         "--load", "", # // config.ini
         "--output", "./static/temp/biotop.gcode", 
         "./static/temp/floor.step", "./static/temp/body.step"])
+
+def upload_gcode() -> requests.Response:
+    with open(FILE_PATH, "rb") as file:
+        return requests.put(
+            f"{PRUSA_CONNECT_API}/api/v1/printers/{PRINTER_ID}/files/",
+            headers={
+                "Authorization": f"Token {PRUSA_CONNECT_API_KEY}",
+                "Content-Type": "application/octet-stream"
+            },
+            data=file
+        )
 
 if __name__ == "__main__":
     import uvicorn
